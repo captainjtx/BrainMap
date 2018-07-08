1c511cd743b74d072cfd0bf65883616887dd0698
Tianxiao Jiang <tj@cerebras.net>
2018-07-08 17:58:20 -0500

    Update MapInterpolationCallback.m

--- src/@BrainMap/MapInterpolationCallback.m
+++ src/@BrainMap/MapInterpolationCallback.m
@@ -2,20 +2,19 @@ function MapInterpolationCallback( obj )
 obj.NotifyTaskStart('Start map interpolation ..');
 interp=obj.JMapInterpolationSpinner.getValue();
 electrode=obj.SelectedElectrode;
+proj_cam_tgt = obj.JMapTriCanvas.isSelected();
 if ~isempty(electrode)
     electrode.coor_interp=interp;
     if is_handle_valid(electrode.map_h)
         
-        newpos=interp_tri(electrode.coor,electrode.coor_interp);
+        
+        newpos=interp_tri(electrode.coor,electrode.coor_interp, proj_cam_tgt);
         
         newmap=electrode.F(newpos(:,1),newpos(:,2),newpos(:,3));
         
-        % use PCA to project 3d points into 2d plane
-        [V, D, ~] = eig(newpos'*newpos);
-        [~, ind] = sort(diag(D));
-        V = V(:, ind);
-        tri=delaunay(newpos*V(:, 2),newpos * V(:,3));
-        
+        [x, y] = project_3d_to_2d( newpos, proj_cam_tgt );
+        tri=delaunay(x, y);
+
         cmapv=zeros(length(newmap),3);
         
         cmin=obj.JMapMinSpinner.getValue();
